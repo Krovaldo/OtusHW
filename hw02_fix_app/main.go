68d821475c9d785cf6fddc69eee1b64119c42b80
Krovaldo <alexcar1801165@gmail.com>
2024-08-18 23:44:15 +0300

    go-mod-fix

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	"github.com/fixme_my_friend/hw02_fix_app/printer"
-	"github.com/fixme_my_friend/hw02_fix_app/reader"
-	"github.com/fixme_my_friend/hw02_fix_app/types"
+	"github.com/Krovaldo/OtusHW/hw02_fix_app/printer"
+	"github.com/Krovaldo/OtusHW/hw02_fix_app/reader"
+	"github.com/Krovaldo/OtusHW/hw02_fix_app/types"
 )
 
 func main() {
