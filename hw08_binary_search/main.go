568dafa95cc494e26fbf9a3caa962c759a1cb517
Krovaldo <alexcar1801165@gmail.com>
2024-10-30 12:23:13 +0300

    Binary Search algorithm

--- hw08_binary_search/main.go
+++ hw08_binary_search/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
+func BinarySearch(arr []int, num int) int {
+	// Сортируем массив
+	sort.Slice(arr, func(i, j int) bool {
+		return arr[i] < arr[j]
+	})
+
+	left := 0
+	right := len(arr) - 1
+
+	for left <= right {
+		middle := (left + right) / 2
+		if arr[middle] == num {
+			return middle
+		}
+		if num < arr[middle] {
+			right = middle - 1
+		}
+		if num > arr[middle] {
+			left = middle + 1
+		}
+	}
+	return -1 // Если число не найдено
+}
+
 func main() {
-	// Place your code here.
+	arr := []int{1, 19, 8, 100, 9, 12, 15}
+	fmt.Println(BinarySearch(arr, 8))
+	fmt.Println(arr)
 }
---
+++ hw08_binary_search/main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBinarySearch(t *testing.T) {
+	testCases := []struct {
+		name     string
+		arr      []int
+		num      int
+		expected int
+	}{
+		{
+			name:     "Число не найдено",
+			arr:      []int{1, 2, 3, 4, 5},
+			num:      100,
+			expected: -1,
+		},
+		{
+			name:     "Пустой срез",
+			arr:      []int{},
+			num:      1,
+			expected: -1,
+		},
+		{
+			name:     "Отсортированный массив",
+			arr:      []int{1, 5, 9, 11, 12, 15},
+			num:      12,
+			expected: 4,
+		},
+		{
+			name:     "Неотсортированный массив",
+			arr:      []int{19, 23, 5, 18, 7},
+			num:      5,
+			expected: 0,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.name, func(t *testing.T) {
+			assert.Equal(t, tC.expected, BinarySearch(tC.arr, tC.num))
+		})
+	}
+}
