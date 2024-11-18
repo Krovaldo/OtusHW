97c5ed12f5897853b0ee5d63ff8377cac20276d1
Krovaldo <alexcar1801165@gmail.com>
2024-11-18 23:03:48 +0300

    Goroutine task

--- hw10_motion_sensor/main.go
+++ hw10_motion_sensor/main.go
@@ -1,5 +1,70 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func readSensorData(dataChan chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	defer close(dataChan)
+
+	src := rand.NewSource(time.Now().UnixNano())
+	getRand := rand.New(src)
+
+	startTime := time.Now()
+
+	for time.Since(startTime) < time.Minute {
+		number := getRand.Intn(1000)
+		dataChan <- number
+		time.Sleep(70 * time.Millisecond)
+	}
+}
+
+func processData(dataChan <-chan int, calculatedData chan<- float64, wg *sync.WaitGroup) {
+	defer wg.Done()
+	defer close(calculatedData)
+
+	count := 0
+	var sum float64
+
+	for number := range dataChan {
+		count++
+		sum += float64(number)
+
+		if count == 10 {
+			avg := sum / 10
+			calculatedData <- float64(avg)
+			count = 0
+			sum = 0
+		}
+	}
+}
+
+func printCalculatedDate(calculatedData <-chan float64) {
+	for avg := range calculatedData {
+		fmt.Printf("Среднее арифметическое: %v\n", avg)
+	}
+}
+
 func main() {
-	// Place your code here.
+	// генератор рандомных чисел
+	// src := rand.NewSource(time.Now().UnixNano())
+	// getRand := rand.New(src)
+	// fmt.Println(getRand.Intn(10)) // рандомайзер от 0 до 10
+
+	dataChan := make(chan int)
+	calculatedData := make(chan float64)
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go readSensorData(dataChan, &wg)
+
+	wg.Add(1)
+	go processData(dataChan, calculatedData, &wg)
+
+	printCalculatedDate(calculatedData) // главная горутина main
 }
---
+++ hw10_motion_sensor/main_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestReadSensor(t *testing.T) {
+	dataChan := make(chan int)
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go readSensorData(dataChan, &wg)
+
+	count := 0
+	timeout := time.After(time.Minute)
+
+	for {
+		select {
+		case <-dataChan:
+			count++
+		case <-timeout:
+			wg.Wait()
+			if count == 0 {
+				t.Error("Данные не были считаны")
+			}
+			return
+		}
+	}
+}
+
+func TestProcessData(t *testing.T) {
+	dataChan := make(chan int, 10)
+	processedDataChan := make(chan float64, 1)
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		dataChan <- i
+	}
+	close(dataChan)
+
+	wg.Add(1)
+	go processData(dataChan, processedDataChan, &wg)
+
+	wg.Wait()
+
+	avg := <-processedDataChan
+	expectedAvg := 4.5
+	if avg != expectedAvg {
+		t.Errorf("Ожидаемое среднее арифметическое: %.2f, полученное: %.2f", expectedAvg, avg)
+	}
+}
